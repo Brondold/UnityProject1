e0f9fe366926a291d36d356d87c9a1eeae8a81e2
Brondold <114911762+Brondold@users.noreply.github.com>
2022-10-23 00:15:05 +0200

    HealthSystem

--- ZeldaLikePROJET/Assets/Scripts/PlayerMovement.cs
+++ ZeldaLikePROJET/Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
+    public FloatValue currentHealth;
+    public SignalSender playerHealthSignal;
 
 
 
@@ -95,9 +97,18 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    public void Knock(float knockTime)
+    public void Knock(float knockTime, float damage)
     {
-        StartCoroutine(KnockCoroutine(knockTime));
+        currentHealth.RuntimeValue -= damage;
+        playerHealthSignal.Raise();
+        if (currentHealth.RuntimeValue > 0)
+        {
+            StartCoroutine(KnockCoroutine(knockTime));
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 
     private IEnumerator KnockCoroutine(float knockTime)
--- ZeldaLikePROJET/Assets/Scripts/ScriptableObjects/FloatValue.cs
+++ ZeldaLikePROJET/Assets/Scripts/ScriptableObjects/FloatValue.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 
 [CreateAssetMenu]
-public class FloatValue : ScriptableObject
+public class FloatValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public float initialValue;
+
+    [HideInInspector]
+    public float RuntimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = initialValue;
+    }
+    public void OnBeforeSerialize()
+    {
+
+    }
 
 }
